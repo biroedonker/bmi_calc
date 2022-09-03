9252b0291106e3461dcae4d140c288937899884b
biroedonker <donkerbiroe@gmail.com>
2022-09-03 15:12:59 +0700

    menambah parameter (major update)

--- bmi_calc.go
+++ bmi_calc.go
@@ -1,5 +1,7 @@
 package bmicalc
 
-func SayHello() string {
+// major update : harus di add push lagi ke repo termasuk go.mod
+
+func SayHello(name string) string {
 	return "Hallo Buooss sudah di update ini"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/biroedonker/bmi_calc
+module github.com/biroedonker/bmi_calc/v2
 
 go 1.19
