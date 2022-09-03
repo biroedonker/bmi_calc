3bd83e6c94090b26da42cc6ea04244e2b40ff46e
biroedonker <donkerbiroe@gmail.com>
2022-09-03 15:22:04 +0700

    menambah return value

--- bmi_calc.go
+++ bmi_calc.go
@@ -3,5 +3,5 @@ package bmicalc
 // major update : harus di add push lagi ke repo termasuk go.mod
 
 func SayHello(name string) string {
-	return "Hallo Buooss sudah di update ini"
+	return "Hallo Buooss sudah di update ini" + name
 }
