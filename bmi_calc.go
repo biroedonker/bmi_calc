f8b92c8b761a4b58c6f31889c5b328d8a9ceef83
biroedonker <donkerbiroe@gmail.com>
2022-09-03 15:40:08 +0700

    menambah spasi pada return

--- bmi_calc.go
+++ bmi_calc.go
@@ -3,5 +3,5 @@ package bmicalc
 // major update : harus di add push lagi ke repo termasuk go.mod
 
 func SayHello(name string) string {
-	return "Hallo Boss" + name + "sudah update return ya"
+	return "Hallo Boss " + name + " sudah update return ya"
 }
